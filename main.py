f98ca4c385c4842adde5f3077315fe856632a288
PrinterPaper101 <nathan.birkett@icsd.k12.ny.us>
2022-04-16 11:09:05 -0400

    converted to using array for square state detection

--- main.py
+++ main.py
@@ -11,16 +11,13 @@ GREEN = (0, 255, 0)
 BLACK = (0, 0, 0)
 WINDOW_WIDTH = 512
 WINDOW_HEIGHT = 512
-increment = WINDOW_WIDTH//16
+increment = WINDOW_WIDTH//512
 WINDOW = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 points = [[False for i in range(WINDOW_WIDTH//increment+1)] for j in range(WINDOW_WIDTH//increment+1)]
 
 def main():
     # print(is_contained(int(input("x:")), int(input("y:"))))
-    print(points[8][8])
     draw()
-    print(points)
-    print(points[8][8])
     looping = True
     while looping :
         for event in pygame.event.get() :
@@ -34,62 +31,44 @@ def draw():
     pygame.draw.line(WINDOW, BLACK, (0, WINDOW_HEIGHT/2), (WINDOW_WIDTH, WINDOW_HEIGHT/2), 2)
     for x in range(-WINDOW_WIDTH//2, WINDOW_WIDTH//2+1, increment):
         for y in range(WINDOW_HEIGHT//2, -WINDOW_HEIGHT//2-1, -increment):
-            pygame.draw.rect(WINDOW, RED, pygame.Rect(gx(x), gy(y), increment, increment), 1)
+            # pygame.draw.rect(WINDOW, RED, pygame.Rect(gx(x), gy(y), increment, increment), 1)
             if is_contained(x, y):
                 # pygame.draw.circle(WINDOW, GREEN, (gx(x), gy(y)), 3)
-                print(str(int(gx(x)/increment)) + ", " + str(int(gy(y)/increment)))
-                if int(gx(x)/increment) == 1 and int(gy(y)/increment) == 8:
-                    print(points[8][8])
                 points[int(gx(x)/increment)][int(gy(y)/increment)] = True
-                if points[8][8] == True:
-                    print("is True")
-            if points[int(gx(x)/increment)][int(gy(y)/increment)] == True:
-                pygame.draw.circle(WINDOW, GREEN, (gx(x), gy(y)), 3)
                 
     for x in range(-WINDOW_WIDTH//2, WINDOW_WIDTH//2, increment):
         for y in range(WINDOW_HEIGHT//2, -WINDOW_HEIGHT//2, -increment):
             if check_box(x, y) == [True, False, False, False]:
                 pygame.draw.line(WINDOW, BLUE, (gx(x), gy(y)+increment/2), (gx(x)+increment/2, gy(y)), 2)
-                    
-            if not is_contained(x, y) and is_contained(x+increment, y) and not is_contained(x, y-increment) and not is_contained(x+increment, y-increment):
+            if check_box(x, y) == [False, True, False, False]:
                 pygame.draw.line(WINDOW, BLUE, (gx(x)+increment/2, gy(y)), (gx(x)+increment, gy(y)+increment/2), 2)
-                    
-            if is_contained(x, y) and is_contained(x+increment, y) and not is_contained(x, y-increment) and not is_contained(x+increment, y-increment):
+            if check_box(x, y) == [True, True, False, False]:
                 pygame.draw.line(WINDOW, BLUE, (gx(x), gy(y)+increment/2), (gx(x)+increment, gy(y)+increment/2), 2)
-                    
-            if not is_contained(x, y) and not is_contained(x+increment, y) and is_contained(x, y-increment) and not is_contained(x+increment, y-increment):
+            if check_box(x, y) == [False, False, True, False]:
                 pygame.draw.line(WINDOW, BLUE, (gx(x), gy(y)+increment/2), (gx(x)+increment/2, gy(y)+increment), 2)
-                    
-            if is_contained(x, y) and not is_contained(x+increment, y) and is_contained(x, y-increment) and not is_contained(x+increment, y-increment):
+            if check_box(x, y) == [True, False, True, False]:
                 pygame.draw.line(WINDOW, BLUE, (gx(x)+increment/2, gy(y)), (gx(x)+increment/2, gy(y)+increment), 2)
                     
                 # if not is_contained(x, y) and is_contained(x+increment, y) and is_contained(x, y-increment) and not is_contained(x+increment, y-increment) and not is_contained(x+increment/2, y-increment/2):
                 #     pygame.draw.line(WINDOW, BLUE, (gx(x)+increment/2, gy(y)), (gx(x)+increment, gy(y)+increment/2), 2)
                 #     pygame.draw.line(WINDOW, BLUE, (gx(x), gy(y)+increment/2), (gx(x)+increment/2, gy(y)+increment), 2)
                     
-            if is_contained(x, y) and is_contained(x+increment, y) and is_contained(x, y-increment) and not is_contained(x+increment, y-increment):
+            if check_box(x, y) == [True, True, True, False]:
                 pygame.draw.line(WINDOW, BLUE, (gx(x)+increment/2, gy(y)+increment), (gx(x)+increment, gy(y)+increment/2), 2)
-                    
-            if not is_contained(x, y) and not is_contained(x+increment, y) and not is_contained(x, y-increment) and is_contained(x+increment, y-increment):
+            if check_box(x, y) == [False, False, False, True]:
                 pygame.draw.line(WINDOW, BLUE, (gx(x)+increment/2, gy(y)+increment), (gx(x)+increment, gy(y)+increment/2), 2)
-                    
                 
                 # if is_contained(x, y) and not is_contained(x+increment, y) and not is_contained(x, y-increment) and is_contained(x+increment, y-increment):
                 #     pygame.draw.line(WINDOW, BLUE, (gx(x), gy(y)+increment/2), (gx(x)+increment/2, gy(y)), 2)
-                    
-            if not is_contained(x, y) and is_contained(x+increment, y) and not is_contained(x, y-increment) and is_contained(x+increment, y-increment):
+            if check_box(x, y) == [False, True, False, True]:
                 pygame.draw.line(WINDOW, BLUE, (gx(x)+increment/2, gy(y)), (gx(x)+increment/2, gy(y)+increment), 2)
-                
-            if is_contained(x, y) and is_contained(x+increment, y) and not is_contained(x, y-increment) and is_contained(x+increment, y-increment):                
+            if check_box(x, y) == [True, True, False, True]:              
                 pygame.draw.line(WINDOW, BLUE, (gx(x), gy(y)+increment/2), (gx(x)+increment/2, gy(y)+increment), 2)
-                
-            if not is_contained(x, y) and not is_contained(x+increment, y) and is_contained(x, y-increment) and is_contained(x+increment, y-increment):
+            if check_box(x, y) == [False, False, True, True]:
                 pygame.draw.line(WINDOW, BLUE, (gx(x), gy(y)+increment/2), (gx(x)+increment, gy(y)+increment/2), 2)
-                
-            if is_contained(x, y) and not is_contained(x+increment, y) and is_contained(x, y-increment) and is_contained(x+increment, y-increment):                
+            if check_box(x, y) == [True, False, True, True]:
                 pygame.draw.line(WINDOW, BLUE, (gx(x)+increment/2, gy(y)), (gx(x)+increment, gy(y)+increment/2), 2)
-                
-            if not is_contained(x, y) and is_contained(x+increment, y) and is_contained(x, y-increment) and is_contained(x+increment, y-increment):
+            if check_box(x, y) == [False, True, True, True]:
                 pygame.draw.line(WINDOW, BLUE, (gx(x), gy(y)+increment/2), (gx(x)+increment/2, gy(y)), 2)
                 
     print("done")
@@ -102,13 +81,11 @@ def gy(y):
     return WINDOW_HEIGHT/2-y
 
 def is_contained(x,y):
-    return math.cos(x) + 10*math.cos(x/10) - y <= 0
+    return x**2 + (y - 8*math.sqrt(abs(x)))**2 - 100**2 <= 0
 
 def check_box(_x,_y):
     x = int(gx(_x)/increment)
     y = int(gy(_y)/increment)
-    # print(_x, _y)
-    # print([points[x][y], points[x+1][y], points[x][y+1], points[x+1][y+1]])
     if [points[x][y], points[x+1][y], points[x][y+1], points[x+1][y+1]] == [True, False, False, False]:
         return [True, False, False, False]
     if [points[x][y], points[x+1][y], points[x][y+1], points[x+1][y+1]] == [False, True, False, False]:
